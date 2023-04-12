7d226ee92b4dd7273bfe143db9bc0069178991ea
sagar1sharma <sagarsharmazzz12@gmail.com>
2023-04-12 15:58:51 +0530

    final build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./app.css";
+import "./App.css";
 import MovieList from "./components/movielist";
 import MovieListHeading from "./components/movielistheading";
 import SearchBox from "./components/searchbox";
@@ -14,7 +14,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+		const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
@@ -28,9 +28,36 @@ const App = () => {
 		getMovieRequest(searchValue);
 	}, [searchValue]);
 
+	useEffect(() => {
+		fetch('https://fav-movie-b549e-default-rtdb.firebaseio.com/movies.json')
+		.then(res => {return res.json()})
+		.then(data => {
+			const movies = [];
+
+			for(const key in data){
+				const meetup = {
+					id: key, 
+					...data[key]
+				};
+				movies.push(meetup);
+			}
+
+			setFavourites(movies);
+		})
+	}, [])
+
 	const addFavouriteMovie = (movie) => {
 		const newFavouriteList = [...favourites, movie];
 		setFavourites(newFavouriteList);
+		fetch('https://fav-movie-b549e-default-rtdb.firebaseio.com/movies.json',
+			{
+				method: 'POST',
+				body: JSON.stringify(movie),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		);
 	};
 
 	const removeFavouriteMovie = (movie) => {
@@ -42,6 +69,7 @@ const App = () => {
 	};
 
 	return (
+		<div>
 		<div className='container-fluid movie-app'>
 			<div className='row d-flex align-items-center mt-4 mb-4'>
 				<MovieListHeading heading='Movies' />
@@ -64,6 +92,8 @@ const App = () => {
 					favouriteComponent={RemoveFavourites}
 				/>
 			</div>
+		</div>
+		<footer><p>@Sagar Sharma</p></footer>
 		</div>
 	);
 };
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Footer(){
-    <footer><p>Copyright- Sagar sharma @2022 </p></footer>
+    <footer><p>Copyright- Sagar sharma @2023 </p></footer>
 }
 
 export default Footer;
